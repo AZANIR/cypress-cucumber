dcadf0b7a4ad33f6edce624aca7c212020735ba8
AZANIR <eleoneks@gmail.com>
2022-08-16 20:06:06 +0300

    up

--- cypress.config.js
+++ cypress.config.js
@@ -3,22 +3,22 @@ const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 async function setupNodeEvents(on, config) {
-  await preprocessor.addCucumberPreprocessorPlugin(on, config);
-  on("file:preprocessor", browserify.default(config));.
-  return config;
-};
+    await preprocessor.addCucumberPreprocessorPlugin(on, config);
+    on("file:preprocessor", browserify.default(config));
+    return config;
+}
 
 module.exports = defineConfig({
-  e2e: {
-    specPattern: "**/*.feature",
-    supportFile: false,
-    setupNodeEvents,
-    devServer: {
-      delay: 500,
-      force404: false,
-      ignore: (xhr) => {
-          return true;
+    e2e: {
+        specPattern: "**/*.feature",
+        supportFile: false,
+        setupNodeEvents,
+        devServer: {
+            delay: 500,
+            force404: false,
+            ignore: (xhr) => {
+                return true;
+            },
+        },
     },
-  },
-  },
 });
